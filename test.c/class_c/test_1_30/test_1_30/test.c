6a57f0e31135d88d2e3cedb97462b7ad07e33742
XING <2954053732@qq.com>
2026-01-31 02:22:02 +0800

    1月30日代码

---
+++ test.c/class_c/test_1_30/test_1_30/test.c
@@ -0,0 +1,139 @@
+#define _CRT_SECURE_NO_WARNINGS 
+#define _CRT_SECURE_NO_WARNINGS 
+//
+//#include<stdio.h>
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int c = 0;
+//    int p = 0;
+//    scanf("%d %d", &a, &b);
+//    c = a / b;
+//    p = a % b;
+//    printf("%d %d", c, p);
+//    return 0;
+//
+//}
+//
+//#include<stdio.h>
+//int main()
+//{
+//    int a = 0;
+//    int b = 0;
+//    int count = 0;
+//    while (scanf("%d %d", &a, &b) != EOF)
+//    {
+//
+//        if (a == b)
+//        {
+//            printf("%d=%d\n", a, b);
+//        }
+//        else if (a > b)
+//        {
+//            printf("%d>%d\n", a, b);
+//        }
+//        else
+//        {
+//            printf("%d<%d\n", a, b);
+//        }
+//
+//    }
+//    return 0;
+//}
+#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d", &a,&b);
+//	c = a + b;
+//	printf("%d", c);
+//	return 0;
+//}
+/*int main()
+{
+	int a = 0;
+	int b = 0;
+	int c = 0;
+	scanf("%d %d", &a, &b);
+	c = a - b;
+	printf("%d", c);
+	return 0;
+}*/
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	scanf("%d %d", &a, &b);
+//	c = a * b;
+//	printf("%d", c);
+//	return 0;
+//}
+/*int main()
+{
+	int  b ;
+	float a ,c;
+	scanf("%f %d", &a,&b);
+	c = a / b;
+	printf("%f", c);
+	return 0;
+
+}*/
+//int main()
+//{
+//	int a, b,c;
+//	scanf("%d %d", &a, &b);
+//	c = a % b;
+//	printf("%d", c);
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = ++a;
+//	printf("%d %d", a, b);
+//	return 0;
+//
+//}
+//int main()
+//{
+//	int a = 3;
+//	int b = a++;
+//	printf("%d %d", a,b);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%5d\n", 123456);//如果是打印有六位但是设置宽度只有五位就会忽略宽度
+//
+//	printf("%5d\n", 1234);  //如果宽度不足没有符号就是靠右面补齐；有符号就是靠左补齐
+//	printf("%-5d\n", 1234);
+//	printf("%5.2f\n", 3.4);//5表示宽度是五而二表示小数点后面只有两位
+//	printf("%.2f\n", 123.456);
+//	printf("%*.*f", 5, 2, 3.4);5填充到第一个* 2填充到第二个*，而3.4代表整体
+//	return 0;
+//}
+//}
+//int main()
+//{
+//	char arr[5];
+//	scanf("%s", arr);
+//	printf("%s", arr);
+//	return 0;
+//}
+//int main()
+//{
+//	printf("%s have %d apples", "zhangming", 3);
+//		return 0; 
+//}
+//int main()
+//{
+//	printf("%s\n", "abcdef");
+//	printf("%5s\n", "abcdefg");//如果打印值超出宽度则会忽略宽度
+//	printf("%.3s\n", "abcdef");//但是在占位符前面加上.几的时候就会按照数字来保留
+//	return 0;
+//}
