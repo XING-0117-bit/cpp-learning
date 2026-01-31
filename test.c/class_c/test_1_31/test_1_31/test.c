d898bb8da142574215544ea4c518ec9c4ac16ce8
XING <2954053732@qq.com>
2026-01-31 23:58:33 +0800

    1月31日代码

---
+++ test.c/class_c/test_1_31/test_1_31/test.c
@@ -0,0 +1,72 @@
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a, b, c, d;
+//	scanf("%d %d", &a, &b);
+//	c = a/b;
+//	d = a % b;
+//	printf("%d %d", c, d);
+//	return 0;
+//
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf("%d", &a);
+//	if (a >= 140)
+//	{
+//		printf("Genius");
+//	}
+//	return 0;
+//}
+////#include<stdio.h>
+////int main()
+////{
+////	int a, b;
+////	while (scanf("%d %d", &a, &b) != EOF)
+////	{
+////		if (a==b)
+////		{
+////			printf("%d=%d\n", a, b);
+////		}
+////
+////		else if (a > b)
+////		{
+////			printf("%d>%d\n", a, b);
+////		}
+////		else
+////		{
+////			printf("%d<%d\n", a, b);
+////		}
+////	}
+////	return 0;
+////}
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	scanf_s("%d", &a);
+//	if (a > 0 && a % 5 == 0)
+//	{
+//		printf("YES");
+//	}
+//	else {
+//		printf("NO");
+//	}
+//	return 0;
+//}
+
+#include<stdio.h>
+int main()
+{
+	int a, i;
+	while(scanf("%d", &a)!=EOF)
+	for (i = 0; i < a; i++)
+	{
+		printf("*");
+	}
+	printf("\n");
+	return 0;
+}
