ac02097e0a24918f718a3e5937e0afd95004d673
XING <2954053732@qq.com>
2026-02-03 23:58:28 +0800

    代码练习

---
+++ test.c/class_c/test_2_2/Project12_01/test.c
@@ -0,0 +1,121 @@
+﻿//例1：输入一个整数判断是否为奇数：
+//#include<stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	scanf_s("%d", &num);
+//	if (num % 2 == 1);
+//	{
+//		printf("%d是奇数\n", num);
+//	}return 0;
+//}
+//例3：输⼊⼀个年龄， >= 18岁就输出：成年，否则就输出：未成年
+//#include<stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	scanf_s("%d", &num);
+//	if (num >= 18)
+//	{
+//		printf("成年");
+//	}
+//	else {
+//		printf("未成年");
+//	}return 0;
+// }
+//例4：要求输⼊⼀个整数，判断输⼊的整数是0，还是正数或者负数。
+//#include<stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	scanf_s("%d", &num);
+//	if (num < 0)
+//	{
+//		printf("%d是负数\n",num);
+//	}
+//	else if (num > 0)
+//	{
+//		printf("%d是正数\n",num);
+//	}
+//	else {
+//		printf("%d是0\n",num);
+//	}return 0;
+//}
+//找最大值：
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int m = 0;
+//	scanf_s("%d,%d", &a, &b);
+//	if (a > b)
+//	{
+//		m = a;
+//	}
+//	else {
+//		m = b;
+//	}printf("%d", m);
+//	return 0;
+// }
+//
+//判断是不是闰年：
+//#include<stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	scanf_s("%d", &num);
+//	if (num % 4 == 0 && num % 100 != 0 || num % 400 == 0)
+//	{
+//		printf("%d是闰年",num);
+//	}return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int num = 0;
+//	scanf_s("%d", &num);
+//	if (num % 3 == 0)
+//	{
+//		printf("余数为0\n");
+//	}
+//	else if (num % 3 == 1)
+//	{
+//		printf("余数为1\n");
+//	}
+//	else {
+//		printf("余数为2\n");
+//	}return 0;
+//}
+//在屏幕上打印1~10；
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	for (i = 0; i <= 10; i++)
+//	{
+//		printf("%d\n", i);
+//	}return 0;
+// }
+//计算1~100之间3的倍数的数字之和
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 0;
+//	int sum = 0;
+//	for(i=0;i<=100;i++)
+//		if (i % 3 == 0)
+//		{
+//			sum += i;
+//		}printf("%d", sum);
+//	return 0;
+//
+//}
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	int sum = 0;
+	for(i=0;i<=n;i++)
+	
+}
