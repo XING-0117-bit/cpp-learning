bf223ff60d14fffc77c013f27c96d82dae2f8a70
XING <2954053732@qq.com>
2026-04-17 22:54:16 +0800

    操作运算符代码

---
+++ test.c/class_c/tets_4_17/tets_4_17/test.c
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 
+//#include<stdio.h>
+//int main()
+//{
+//	int n = 10;
+//	int m = n << 1;
+//	printf("n=%d\n", n);
+//	printf("m=%d\n", m);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int n = -10;
+//	int m = n << 1;
+//	printf("n=%d\n", n);
+//	printf("m=%d\n", m);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//
+//{
+//	int n = 10;
+//	int m = n >> 1;
+//	printf("n=%d\n", n);
+//	printf("m=%d\n", m);
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int n = -10;
+	int m = n >> 1;
+	printf("n=%d\n", n);
+	printf("m=%d\n", m);
+	return 0;
+}
