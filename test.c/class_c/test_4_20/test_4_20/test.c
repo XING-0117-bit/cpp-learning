7988af3c981cfcf191e57094e81a954444b67267
XING <2954053732@qq.com>
2026-04-20 22:54:42 +0800

    操作符代码

---
+++ test.c/class_c/test_4_20/test_4_20/test.c
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS 
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,1,2,3,4 };
+//	int sum = 0;
+//	int n = sizeof(arr) / sizeof(arr[0]);
+//	for (int i = 0; i < n; i++)
+//	{
+//		sum^=arr[i];
+//	}
+//	printf("%d", sum);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a = 3;
+//	int b = 0;
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	printf("a=%d,b=%d", a, b);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    int n = 0;
+//    scanf("%d", &n);
+//    int count = 0;
+//    if (n & (n - 1) == 1)
+//    {
+//        count++;
+//    }
+//    printf("%d", count);
+//    return 0;
+//}
+#include<stdio.h>
+int main()
+{
+	int n = 0;
+	scanf("%d", &n);
+	int m = 0;
+	int count = 0;
+	if(m&n==)
+
+}
