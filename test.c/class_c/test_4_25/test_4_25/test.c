2f636a6e9fe89082aa9bab50aab7badd8b0866aa
XING <2954053732@qq.com>
2026-04-25 23:53:15 +0800

    指针代码

---
+++ test.c/class_c/test_4_25/test_4_25/test.c
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int* p = &arr[0];
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%d ", *p);
+//		p++;
+//	}
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int* p = &arr[sz - 1];
+//	for (int i = 0; i < sz; i++)
+//	{
+//		printf("%d", *p);
+//		p--;
+//	}
+//	return 0;
+//}
+#include<stdio.h>
+int main()
+{
+
+	int arr[] = { 1,2,3,4,5,6,7,8 };
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	int* p = &arr[0];
+	for (int i = 0; i < sz; i++)
+	{
+		printf("%d", *(p + i));
+	}
+	return 0;
+}
